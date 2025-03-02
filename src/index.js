31522b051fba035c48fddbfee4ea349d9749a1a6
kanade <pipicount@gmail.com>
2025-03-02 20:51:01 +0900

    servise-worker.jsファイルの追加

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
 // Service Worker の登録
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+    navigator.serviceWorker.register('/time-recorder-app/service-worker.js').then(function(registration) {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
       console.log('ServiceWorker registration failed: ', err);
